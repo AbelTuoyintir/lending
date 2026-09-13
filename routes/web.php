dbda8906e542498f9c8f2a1cdb326d7effa8d5f8
AbelTuoyintir <abeltuoyintir07@gamil.com>
2026-09-13 20:38:35 +0000

    hiwsfr

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ use App\Http\Controllers\StatementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
-});
+    return redirect()->route('login');
+})->name('home');
 
 /*
 |--------------------------------------------------------------------------
